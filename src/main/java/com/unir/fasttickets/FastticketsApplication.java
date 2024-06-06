5315db590343863fd8fc9061664412d67155d75e
Alexander Ahumada <alex.ahusoto@gmail.com>
2024-06-05 20:46:19 -0500

    Update

---
+++ src/main/java/com/unir/fasttickets/FastticketsApplication.java
@@ -0,0 +1,13 @@
+package com.unir.fasttickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastticketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastticketsApplication.class, args);
+	}
+
+}
